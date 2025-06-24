f5ee2dea50cf123d97aa94cb909e106750a105e7
Lkz30 <luiscanoayestas@gmail.com>
2025-06-24 07:56:50 +0200

    Initial Commit

---
+++ PiedraPapelOTijera/PiedraPapelOTijeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiedraPapelOTijeraApp.swift
+//  PiedraPapelOTijera
+//
+//  Created by Luis Cano  on 2025-06-24.
+//
+
+import SwiftUI
+
+@main
+struct PiedraPapelOTijeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
